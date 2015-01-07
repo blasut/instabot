c561db7d51b8dc36325bb979c7b9ce8a80a97732
oklaridentitet <oklaridentitet@gmail.com>
2015-01-07 17:47:28 +0100

    add more future tests

--- test/instabot/core_test.clj
+++ test/instabot/core_test.clj
@@ -1,4 +1,9 @@
 (ns instabot.core-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [instabot.core :refer :all]))
 
+(future-fact "proper-start-date-for-spaning"
+        (fact "it should return the spaning-start date if media-start-date is nil")
+        (fact "it should return the media-start-date if spaning start date is empty")
+        (fact "it should return the spaning-start-date if that is sooner than media-start-date")
+        (fact "it should return the media-start-date if that is sooner than spaning-start-date"))
