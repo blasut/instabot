b2f62f31d4ba89a438e240a853b2d9d3db457aff
oklaridentitet <oklaridentitet@gmail.com>
2015-02-06 09:08:40 +0100

    indent shit in insta

--- src/instabot/insta.clj
+++ src/instabot/insta.clj
@@ -63,7 +63,7 @@
 (defn within-time-range [media stop-date]
   (filter (fn [image]
             (> (use-correct-time-zone (fix-create-time-string image))
-                         stop-date )) media))
+               stop-date )) media))
 
 (defn fix-date [date]
   (let [default-tl (use-correct-time-zone (tc/to-long (t/epoch)))
